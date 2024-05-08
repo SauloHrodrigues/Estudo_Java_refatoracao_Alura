26feb711cc32b518f72edb4e911b4076523f6cdb
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-05-08 10:52:46 -0300

    Criada a classe dto para a entidade adoção e adaptações na classe service

---
+++ src/main/java/br/com/alura/adopet/api/dto/adocao/ReprovacaoAdocaoDto.java
@@ -0,0 +1,4 @@
+package br.com.alura.adopet.api.dto.adocao;
+
+public record ReprovacaoAdocaoDto(Long idAdocao, String justificativa) {
+}
--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -1,13 +1,17 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.dto.adocao.AprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.adocao.ReprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.adocao.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoExcepition;
 import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,32 +19,40 @@ import java.util.List;
 
 public class AdocaoService {
     @Autowired
-    private AdocaoRepository repository;
+    private AdocaoRepository adocaoRepository;
+    @Autowired
+    private PetRepository petRepository;
+
+    private TutorRepository tutorRepository
 
     @Autowired
     private EmailSevice emailSevice;
 
-    public void solicitar(Adocao adocao){
-        if (adocao.getPet().getAdotado() == true) {
+    public void solicitar(SolicitacaoAdocaoDto dto){
+        Pet pet= petRepository.getReferenceById(dto.idPet());
+        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
+
+//        if (adocao.getPet().getAdotado() == true) { 3:59
+        if (pet.getAdotado() == true) {
             throw new ValidacaoExcepition("Pet já foi adotado!");
 //            return ResponseEntity.badRequest().body("Pet já foi adotado!");
         } else {
-            List<Adocao> adocoes = repository.findAll();
+            List<Adocao> adocoes = adocaoRepository.findAll();
             for (Adocao a : adocoes) {
-                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
                     throw new ValidacaoExcepition("Tutor já possui outra adoção aguardando avaliação!");
 //                    return ResponseEntity.badRequest().body("Tutor já possui outra adoção aguardando avaliação!");
                 }
             }
             for (Adocao a : adocoes) {
-                if (a.getPet() == adocao.getPet() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
                     throw new ValidacaoExcepition("Pet já está aguardando avaliação para ser adotado!");
 //                    return ResponseEntity.badRequest().body("Pet já está aguardando avaliação para ser adotado!");
                 }
             }
             for (Adocao a : adocoes) {
                 int contador = 0;
-                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.APROVADO) {
+                if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.APROVADO) {
                     contador = contador + 1;
                 }
                 if (contador == 5) {
@@ -49,9 +61,13 @@ public class AdocaoService {
                 }
             }
         }
+        Adocao adocao = new Adocao();
         adocao.setData(LocalDateTime.now());
         adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
-        repository.save(adocao);
+        adocao.setPet(pet);
+        adocao.setTutor(tutor);
+        adocao.setMotivo(dto.motivo());
+        adocaoRepository.save(adocao);
         emailSevice.enviarEmail(adocao.getPet().getAbrigo().getEmail(),
                 "Solicitação de adoção",
                 "Olá " + adocao.getPet().getAbrigo().getNome() +
@@ -59,7 +75,8 @@ public class AdocaoService {
                         adocao.getPet().getNome() + ". Favor avaliar para aprovação ou reprovação."   );
     }
 
-    public void aprovar(Adocao adocao){
+    public void aprovar(AprovacaoAdocaoDto dto){
+        Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
         adocao.setStatus(StatusAdocao.APROVADO);
         emailSevice.enviarEmail(adocao.getPet().getAbrigo().getEmail(),
                 "Adoção aprovada",
@@ -70,16 +87,17 @@ public class AdocaoService {
                         adocao.getPet().getAbrigo().getNome() + " para agendar a busca do seu pet.");
     }
 
-    public void reprovar(Adocao adocao){
+    public void reprovar(ReprovacaoAdocaoDto dto){
+        Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
         adocao.setStatus(StatusAdocao.REPROVADO);
-        repository.save(adocao);
+//        adocaoRepository.save(adocao); não precisa
         emailSevice.enviarEmail(adocao.getPet().getAbrigo().getEmail(),
                 "Adoção reprovada",
                 "Olá " + adocao.getTutor().getNome() + "!\n\nInfelizmente sua adoção do pet " +
                         adocao.getPet().getNome() + ", solicitada em " +
                         adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +
                         ", foi reprovada pelo abrigo " + adocao.getPet().getAbrigo().getNome() +
-                        " com a seguinte justificativa: " + adocao.getJustificativaStatus());
+                        " com a seguinte justificativa: " + adocao.getJustificativaStatus());//#!$@Ulo1978
 
     }
 }
